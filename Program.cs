6ae80c0d467cd7e3bd7227bab356b40a0b2a20f3
Toreole <toreole.official@gmail.com>
2024-04-05 12:44:53 +0200

    Core Service and Entry Point

--- Program.cs
+++ Program.cs
@@ -1,10 +1,50 @@
-﻿namespace WeatherChecker
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.DependencyInjection;
+using OpenMeteo;
+using Quartz;
+using Quartz.Impl;
+using System.Configuration;
+using System.Globalization;
+using WeatherChecker.Data;
+using WeatherChecker.Jobs;
+using WeatherChecker.ScheduledJobs;
+
+namespace WeatherChecker;
+
+public class Program
 {
-	internal class Program
+	static void Main(string[] _)
+		=> Run().GetAwaiter().GetResult();
+	
+	static async Task Run()
 	{
-		static void Main(string[] args)
+		//force into en-US culture for standardizing to decimal numbers using a . 
+		CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+		UpdateDatabaseSchema();
+		await new WeatherCheckerService().Run();
+		await Task.Delay(-1);
+	}
+
+	/// <summary>
+	/// Apply all pending migrations to the database.
+	/// </summary>
+	static void UpdateDatabaseSchema()
+	{
+		using WeatherDBContext db = new();
+		Console.WriteLine(db.Database.ProviderName);
+		if (db.Database.CanConnect() == false)
 		{
-			Console.WriteLine("Hello, World!");
+			Console.WriteLine("Failed to connect to database.");
+			return;
+		}
+		var migrator = db.GetInfrastructure().GetService<IMigrator>();
+		foreach (var migration in db.Database.GetPendingMigrations())
+		{
+			Console.WriteLine($"Applying migration {migration}");
+			migrator?.Migrate(migration);
 		}
 	}
+
 }
---
+++ WeatherCheckerService.cs
@@ -0,0 +1,65 @@
+﻿using Quartz.Impl;
+using Quartz;
+using System.Configuration;
+using WeatherChecker.ScheduledJobs;
+using WeatherChecker.Jobs;
+
+namespace WeatherChecker;
+
+internal class WeatherCheckerService
+{
+	internal static readonly WeatherConfig config;
+
+	private IScheduler? scheduler;
+
+	static WeatherCheckerService()
+	{
+		var settings = ConfigurationManager.AppSettings;
+		config = new WeatherConfig(
+			float.Parse(settings.Get("latitude") ?? "0"),
+			float.Parse(settings.Get("longitude") ?? "0"),
+			int.Parse(settings.Get("forecast_days") ?? "0")
+		);
+	}
+
+	public async Task Run(CancellationToken cancellationToken = default)
+	{
+		scheduler = await new StdSchedulerFactory()
+			.GetScheduler(cancellationToken);
+
+		await scheduler.Start(cancellationToken);
+		//set up the jobs.
+		var job = JobBuilder.Create<CurrentWeatherJob>()
+			.Build();
+		var trigger = TriggerBuilder.Create()
+			.StartNow()
+			.WithCronSchedule("0 0 * * * ?") //0 seconds, 0 minutes, every hour, every day
+			.Build();
+		await scheduler.ScheduleJob(job, trigger, cancellationToken);
+
+		job = JobBuilder.Create<DailyForecastJob>()
+			.Build();
+		trigger = TriggerBuilder.Create()
+			.StartNow()
+			.WithCronSchedule("5 0 12 * * ?") //5 seconds, 0 minutes, 12:00, every day
+			.Build();
+		await scheduler.ScheduleJob(job, trigger, cancellationToken);
+
+	}
+
+	public async Task Shutdown() 
+		=> await (scheduler?.Shutdown() ?? Task.Delay(1));
+
+	internal readonly struct WeatherConfig
+	{
+		internal readonly float latitude, longitude;
+		internal readonly int forecastDays;
+
+		internal WeatherConfig(float lat, float lon, int days)
+		{
+			latitude = lat;
+			longitude = lon;
+			forecastDays = Math.Clamp(days, 1, 16);
+		}
+	}
+}
